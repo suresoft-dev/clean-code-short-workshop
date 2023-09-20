461b173557afb0e1279464f4d4a76234e526a0f3
Sven Marcus <s.marcus@outlook.de>
2023-09-20 16:40:41 +0200

    Refactored into functions

--- temperaturelog/__main__.py
+++ temperaturelog/__main__.py
@@ -16,22 +16,42 @@ class TemperatureData:
         return TemperatureData(datetime.fromisoformat(dt), float(kelvin))
 
 
-def write_celsius_values_in_timeframe_to_log(start: datetime, end: datetime) -> None:
-    data = []
-    for line in Path("values.csv").read_text().splitlines():
-        data.append(TemperatureData.from_csv(line))
+INPUT_FILE = Path("values.csv")
+RESULT_FILE = Path("logfile.txt")
 
-    celsius_values: list[float] = []
-    for d in data:
-        if not math.isnan(d.value_in_kelvin):
-            if start < d.datetime < end:
-                celsius_values.append(d.value_in_kelvin - 273.15)
 
-    Path("logfile.txt").write_text("\n".join(map(str, celsius_values)))
+def read_temperature_data(path: Path) -> list[TemperatureData]:
+    return [TemperatureData.from_csv(line) for line in path.read_text().splitlines()]
+
+
+def is_valid_value(d: TemperatureData) -> bool:
+    return not math.isnan(d.value_in_kelvin)
+
+
+def is_in_timeframe(d: TemperatureData, start: datetime, end: datetime) -> bool:
+    return start < d.datetime < end
+
+
+def celsius(d: TemperatureData) -> float:
+    return d.value_in_kelvin - 273.15
+
+
+def celsius_values_in_timeframe(
+    data: list[TemperatureData], start: datetime, end: datetime
+) -> list[float]:
+    return [
+        celsius(d) for d in data if is_valid_value(d) and is_in_timeframe(d, start, end)
+    ]
+
+
+def write_results(result_file: Path, values: list[float]) -> None:
+    result_file.write_text("\n".join(map(str, values)))
 
 
 START_DATE = datetime(2023, 9, 20, hour=8)
 END_DATE = START_DATE + timedelta(hours=2)
 
 if __name__ == "__main__":
-    write_celsius_values_in_timeframe_to_log(START_DATE, END_DATE)
+    data = read_temperature_data(INPUT_FILE)
+    celsius_values = celsius_values_in_timeframe(data, START_DATE, END_DATE)
+    write_results(RESULT_FILE, celsius_values)
